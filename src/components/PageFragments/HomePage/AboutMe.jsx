72011655e23e7dd67e1c83396fd4fb3f8f35ca96
Shaheer Ahmed <62714282+boringcuriosity@users.noreply.github.com>
2020-05-30 16:18:32 +0530

    Update AboutMe.jsx

--- src/components/PageFragments/HomePage/AboutMe.jsx
+++ src/components/PageFragments/HomePage/AboutMe.jsx
@@ -5,16 +5,13 @@ import { stripTags, domHtml } from '../../../utils/stripTags';
 import SEO from '../../Seo';
 
 const pageText = {
-  paraOne: `Hello !! My name is Rolwin Reevan Monteiro. I'm a full stack web developer who is
+  paraOne: `Hello !! My name is <b>Mohammed Shaheer Ahmed</b>. I'm a Digital creator who is
     passionate about various web technologies. I like to experiment with different web
-    technologies. I have an experience of nearly 3 years working with LAMP stack, MERN stack
-    and ELK stack. Building fancy UI's just like this one that your seeing 😅 and writing blogs about tech stacks
-    is what Rolwin loves to do. Check my blog which I update every week for some Javascript and some 
+    technologies. I have experience in creating different types of websites and stuff and mostly without code 😅 and writing blogs about tech stacks
+    is what I love to do. Check my blog which I update every week for some work and some 
     cool notes on web technologies.`,
-  paraTwo: `Currently I work mostly with Javascript technologies like ReactJS and NodeJS. I also
-    have hands on experience working with cloud infrastructures like <b>AWS/GCP</b> and have deployed applications
-    keeping scalability in mind. Docker, Kubernetes, Jenkins, SonarQube are some of the cool
-    tools I use for <b>CI/ CD</b>. I'm always a learner and a self taught programmer.`,
+  paraTwo: `Currently I work mostly on my latest ed-tech startup <b>Unmap<b/>. I have already built tons of other projects and will be soon launching few of them.
+I'm always a learner and a self taught <b>solopreneur</b>.`,
 };
 const AboutMe = () => {
   const description = `${pageText.paraOne} ${stripTags(pageText.paraTwo)}`;
@@ -40,15 +37,15 @@ const AboutMe = () => {
             height={60}
             alt="location image"
             textH4="Born and bought up in"
-            textH3="Mangalore, KA, India"
+            textH3="Telangana, India"
           />
         </Col>
         <Col xs={24} sm={24} md={12} lg={8}>
           <AboutTile
             img="coffee.png"
             alt="coffee image"
-            textH4="Love Coffee"
-            textH3="Coffee + Me = Happiness"
+            textH4="Love Tea"
+            textH3="Tea + Me = Happiness"
           />
         </Col>
         <Col xs={24} sm={24} md={12} lg={8}>
@@ -63,15 +60,15 @@ const AboutMe = () => {
           <AboutTile
             img="motorcycle.png"
             alt="motorcycle image"
-            textH4="Love Riding"
-            textH3="Biker for life"
+            textH4="Love Travelling"
+            textH3="Traveller for life"
           />
         </Col>
         <Col xs={24} sm={24} md={12} lg={8}>
           <AboutTile
             img="web.png"
             alt="web image"
-            textH4="Self Taught Programmer"
+            textH4="Self Taught solopreneur"
             textH3="Thanks to the Web Resources"
             height={60}
             width={60}
@@ -81,7 +78,7 @@ const AboutMe = () => {
           <AboutTile
             img="graduation.png"
             alt="graduation image"
-            textH4="Pursued B.Tech in"
+            textH4="Pursuing Intermediate"
             textH3="Computer Science"
             height={60}
             width={60}
